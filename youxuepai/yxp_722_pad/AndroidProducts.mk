15635b319856767e3de7b255d693e3e4052587fc
jbcaaa <131173089+jbcaaa@users.noreply.github.com>
2023-07-22 14:05:21 +0800

    Add files via upload

---
+++ youxuepai/yxp_722_pad/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_yxp_722_pad.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_yxp_722_pad-user \
+    omni_yxp_722_pad-userdebug \
+    omni_yxp_722_pad-eng
